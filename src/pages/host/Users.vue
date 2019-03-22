7ff761120c9997087c61737e3d0ac0b82eaf93c3
George <georgej1088@gmail.com>
2019-03-22 18:38:56 -0400

    filter the user list

--- src/pages/host/Users.vue
+++ src/pages/host/Users.vue
@@ -1,16 +1,22 @@
 <template>
-	<table v-if="loaded">
-		<tr>
-			<th v-for="(value, key) in users[0]" :key="key">
-				{{key}}
-			</th>
-		</tr>
-		<tr v-for="user in users" :key="user.name">
-			<td v-for="(value, key) in user" :key="user.name + key">
-				{{value}}
-			</td>
-		</tr>
-	</table>
+	<div v-if="loaded">
+		<div class="filter">
+			Filter:
+			<input v-model="filter" placeholder="/u/redditname, @Discord Name"/>
+		</div>
+		<table>
+			<tr>
+				<th v-for="(value, key) in users[0]" :key="key">
+					{{key}}
+				</th>
+			</tr>
+			<tr v-for="user in filteredUsers" :key="user.name">
+				<td v-for="(value, key) in user" :key="user.name + key">
+					{{value}}
+				</td>
+			</tr>
+		</table>
+	</div>
 	<h1 v-else>Loading...</h1>
 </template>
 
@@ -20,7 +26,14 @@ export default {
 		return {
 			users: [],
 			loaded: false,
+			filter: '',
 		};
+	},
+	computed: {
+		filteredUsers () {
+			if (!this.filter) return this.users;
+			return this.users.filter(user => `/u/${user.reddit}`.toLowerCase().includes(this.filter));
+		},
 	},
 	created () {
 		fetch('/api/users').then(res => res.json()).then(users => {
