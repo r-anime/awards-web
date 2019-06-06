32b854a3beab6a54c3447167e32eb14f7512e3f5
George <georgej1088@gmail.com>
2019-06-05 21:09:20 -0400

    log correct URLs on hit

--- util/logging.js
+++ util/logging.js
@@ -1,6 +1,6 @@
 const log = require('another-logger');
 
 module.exports = (request, response, next) => {
-	log.hit(`${request.method} ${request.path}`);
+	log.hit(`${request.method} ${request.originalUrl}`);
 	next();
 };
