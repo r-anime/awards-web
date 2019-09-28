20610c8df261028e319afb7dc2b6615aa728aeaf
George <georgej1088@gmail.com>
2019-09-28 17:03:40 -0400

    Prepare for saving show entries soon:tm:

--- frontend/components/CategoryEntries.vue
+++ frontend/components/CategoryEntries.vue
@@ -3,16 +3,25 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 import ShowPicker from './ShowPicker';
 
 export default {
 	components: {
 		ShowPicker,
 	},
+	props: {
+		category: Object,
+	},
 	data () {
 		return {
 			selections: [],
 		};
+	},
+	methods: {
+		...mapActions([
+			// 'updateCategoryEntries', // TODO not implemented
+		]),
 	},
 };
 </script>
