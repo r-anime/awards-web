a2b427da4fe162da2f1acbb900f1cfbab6e6ada3
George <georgej1088@gmail.com>
2019-09-28 18:55:33 -0400

    sAVING ENTRIES WORKS NOW>???

--- frontend/components/CategoryEntries.vue
+++ frontend/components/CategoryEntries.vue
@@ -1,5 +1,22 @@
 <template>
-	<show-picker v-model="selections"/>
+	<div>
+		<show-picker
+			v-if="category.entryType === 'shows'"
+			v-model="selections"
+		/>
+		<h2 class="title" v-else>
+			This is a non-show category, editing is not yet supported
+		</h2>
+
+		<hr>
+		<button
+			class="button is-success"
+			:class="{'is-loading': submitting}"
+			@click="submit"
+		>
+			Update Entries
+		</button>
+	</div>
 </template>
 
 <script>
@@ -15,13 +32,37 @@ export default {
 	},
 	data () {
 		return {
-			selections: [],
+			selections: JSON.parse(this.category.entries),
+			submitting: false,
 		};
 	},
 	methods: {
 		...mapActions([
-			// 'updateCategoryEntries', // TODO not implemented
+			'updateCategory',
 		]),
+		async submit () {
+			this.submitting = true;
+			try {
+				await this.updateCategory({
+					id: this.category.id,
+					data: {
+						entries: JSON.stringify(this.selections),
+					},
+				});
+			} finally {
+				this.submitting = false;
+			}
+		},
 	},
+	watch: {
+		category (newVal, oldVal) {
+			console.log('category changed', newVal, oldVal);
+			// If the category was already defined, we don't want to update the
+			// other data, because the user may be making changes
+			if (oldVal) return;
+			this.selections = JSON.parse(newVal.entries);
+		},
+	},
+
 };
 </script>
