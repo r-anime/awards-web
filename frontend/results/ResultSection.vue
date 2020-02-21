e58f2b94027a598afdc9c7b091e50e60ac088f51
EpicTroll1365 <acciowand200@hotmail.com>
2020-02-21 08:28:36 +0200

    resize containers etc.

--- frontend/results/ResultSection.vue
+++ frontend/results/ResultSection.vue
@@ -1,32 +1,38 @@
 <template>
-    <div :id="slug" class="awardSectionContainer container">
-        <div class="awardSectionHeader has-text-centered has-text-light">
-            <div class="sectionIconContainer">
-				<fa-icon :icon="icon" size="6x" class="has-text-gold mb-20 mt-20" />
+	<section class="hero is-fullheight-with-navbar">
+		<div class="columns is-centered">
+			<div class="column is-half">
+				<div :id="slug" class="awardSectionContainer container">
+					<div class="awardSectionHeader has-text-centered has-text-light">
+						<div class="sectionIconContainer">
+							<fa-icon :icon="icon" size="6x" class="has-text-gold mb-20 mt-20" />
+						</div>
+						<h2 class="sectionHeader title is-2 has-text-light pb-20 has-flaired-underline">{{section.name}} Awards</h2>
+						<div
+						v-if="typeof section.blurb === 'string'"
+						class="awardSectionBlurb"
+						html="blurb"
+						/>
+						<div v-else>
+							<div
+							v-for="(blurb, index) in section.blurb"
+							class="awardSectionBlurb"
+							:key="index"
+							v-html="blurb"
+							/>
+						</div>
+					</div>
+					<awards-category
+					v-for="(category, index) in section.awards"
+					:key="index"
+					:category="category"
+					:data="data"
+					:anilistData="(category.entryType === 'shows' || category.entryType === 'themes') ? showData : charData"
+					/>
+				</div>
 			</div>
-            <h2 class="sectionHeader title is-2 has-text-light pb-20 has-flaired-underline">{{section.name}} Awards</h2>
-            <div
-                v-if="typeof section.blurb === 'string'"
-                class="awardSectionBlurb"
-                html="blurb"
-            />
-            <div v-else>
-                <div
-                    v-for="(blurb, index) in section.blurb"
-                    class="awardSectionBlurb"
-                    :key="index"
-                    v-html="blurb"
-                />
-            </div>
-        </div>
-        <awards-category
-            v-for="(category, index) in section.awards"
-            :key="index"
-            :category="category"
-			:data="data"
-			:anilistData="(category.entryType === 'shows' || category.entryType === 'themes') ? showData : charData"
-        />
-    </div>
+		</div>
+	</section>
 </template>
 
 <script>
