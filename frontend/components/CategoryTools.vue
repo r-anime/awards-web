a51a874adc83281798219a20e2e2b9ff5c2893df
EpicTroll1365 <acciowand200@hotmail.com>
2020-02-21 08:44:05 +0200

    change json schema again

--- frontend/components/CategoryTools.vue
+++ frontend/components/CategoryTools.vue
@@ -309,9 +309,11 @@ export default {
 									id,
 									altname: nom.alt_name,
 									altimg: '',
-									public: index + 1,
+									public: nom.votes,
+									finished: nom.finished / totalVotes,
+									support: nom.votes / nom.finished,
 									jury: nom.rank,
-									percent: `${(nom.votes / totalVotes) * 100}%`,
+									percent: nom.votes / totalVotes,
 									writeup: nom.writeup,
 									staff: nom.staff,
 								});
