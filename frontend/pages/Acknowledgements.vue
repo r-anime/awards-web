313a7d193005e7d9e23227899add68f717af576e
EpicTroll1365 <acciowand200@hotmail.com>
2020-02-22 20:27:31 +0200

    acknowledgements and about

--- frontend/pages/Acknowledgements.vue
+++ frontend/pages/Acknowledgements.vue
@@ -7,9 +7,110 @@
                     <div class="column is-two-thirds-desktop is-three-quarters-tablet is-four-fifths-mobile has-background-black-bis">
                         <section class="section">
                             <div class="container">
-                                <h1 class="title is-3 has-text-gold has-text-left">Acknowledgements</h1>
+                                <h1 class="title is-3 has-text-gold has-text-left has-flaired-underline pb-20">Acknowledgements</h1>
+								<h1 class="title is-5 has-text-gold has-text-centered has-flaired-underline pb-20">Tetsuo Hirakawa</h1>
                                 <div class="has-text-justified has-text-periwinkle">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus leo, elementum et odio ut, pretium sodales lorem. Aliquam condimentum nisl id ullamcorper porttitor. Sed et augue et urna sodales euismod id eu lorem. Duis varius diam a nunc hendrerit ullamcorper. Maecenas sodales libero ac quam dignissim, in pellentesque felis mollis. Praesent et congue ex. Nunc lacinia ipsum ex, sit amet hendrerit neque tincidunt porta. Duis viverra erat id purus posuere, quis dignissim elit posuere. Aenean auctor tellus nisl, nec volutpat urna ultricies sed. Vestibulum imperdiet laoreet tincidunt. Phasellus malesuada nunc auctor, porta nibh non, dictum purus. Maecenas hendrerit nisi in enim viverra, eget dignissim diam egestas. Proin bibendum sodales sapien. Vivamus augue tellus, molestie vitae dui ornare, finibus euismod magna. Aenean imperdiet sed massa ut sodales. Sed semper magna nunc, vitae volutpat nibh blandit id. Morbi auctor malesuada lorem non lacinia. Fusce in libero at enim imperdiet posuere id ac tortor. Fusce consectetur viverra turpis vitae dignissim. Curabitur ornare, enim sed ultricies cursus, nisl dui tempus purus, sit amet porttitor ligula justo quis ante. Morbi blandit mauris sed ex vestibulum ultrices. Vestibulum varius non lectus et mattis. Donec quis sagittis tellus. Curabitur pretium eget sem non ullamcorper. Aenean feugiat hendrerit urna, eu bibendum elit tincidunt at. In vestibulum gravida massa in accumsan. Proin ac sagittis nisl. Morbi viverra laoreet fermentum. Nulla et quam vel velit malesuada eleifend eget non neque. Donec posuere faucibus urna, at iaculis mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec vehicula enim. Suspendisse a justo gravida, auctor enim a, volutpat dolor. Vivamus consectetur ipsum nec condimentum scelerisque. Aliquam tincidunt in ex at porttitor. Nam sem dolor, imperdiet et sem at, semper auctor ante. Vestibulum a tincidunt libero, in imperdiet dui. Nullam quam orci, sollicitudin a luctus et, placerat eget ante. Curabitur efficitur, turpis eget luctus euismod, quam nulla dapibus odio, vitae tincidunt nisi est non mi. Vestibulum et sem a massa cursus vehicula ut sit amet sem. Integer at cursus orci. Etiam et massa condimentum, pretium enim sit amet, porttitor augue. Maecenas quis ex vehicula, tristique nisl quis, elementum tellus. Praesent ultricies diam lacus, consectetur imperdiet dui facilisis a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam consequat nunc urna, a tempor risus ultrices nec. Aenean vitae arcu et ligula sollicitudin eleifend. Integer consequat dignissim tempus. Praesent feugiat erat dolor, a mattis augue porta eu. Mauris molestie id tortor congue cursus. Nullam libero nisl, commodo iaculis lacus vel, feugiat congue purus. Donec ut diam vitae quam luctus congue placerat vel quam. Donec vestibulum vestibulum nisl, ac molestie ante egestas id. Cras sit amet urna justo. Nam at ligula eget tellus consequat bibendum ut non libero. Morbi hendrerit faucibus lacus et venenatis. Proin elit leo, tristique ac fermentum in, pharetra id risus. Aenean tempor in massa sed dignissim. In suscipit consectetur sem, aliquam imperdiet arcu posuere vitae. Etiam eget sem ut ex tempor finibus ut ac nisl. Etiam mollis id sem non laoreet. Vivamus a vulputate leo, non dictum purus. Nulla pharetra augue id nunc fermentum, sit amet tempor ex dignissim. Donec elementum posuere dictum. Praesent vestibulum efficitur erat sit amet convallis. Donec placerat urna sed mi tristique semper.
+                                    コメント：
+									<br/>
+                                    みなさん、はじめまして。
+									<br/>
+                                    アニメ『けだまのゴンじろー』監督の平川哲生です。
+									<br/>
+                                    この度は『わさわさわさ！』が Reddit 2019 Anime Awards のED賞をいただけて、大変うれしく思います。
+									<br/>
+                                    私は、気持ちが言語を超えて伝わるところがアニメーションのすばらしさだと信じております。
+									<br/>
+                                    その私の信念が、この受賞で証明されました。
+									<br/>
+                                    ありがとうございました！
+                                </div>
+								<br/>
+								<div class="has-text-justified has-text-periwinkle">
+									Hello everyone. I'm Tetsuo Hirakawa, director of the anime Kedama no Gonjiro.
+									<br/>
+									I am incredibly happy to have received the ED award for Wasa Wasa Wasa! at these Reddit 2019 Anime Awards.
+									<br/>
+									I believe that the beautiful thing about animation is that it can transcend language to convey emotions. My belief has been proven true by winning this award. Thank you very much!
+                                </div>
+								<br/>
+								<h1 class="title is-5 has-text-gold has-text-centered has-flaired-overunder pb-20">Kenichiro Suehiro</h1>
+								<div class="has-text-justified has-text-periwinkle">
+									みなさん、『とつくにの少女』をご覧いただき、本当にありがとうございます。
+									<br/>
+									できる限り、原作の雰囲気を大切に作った一本です。10分という短い作品ですが、
+									<br/>
+									みなさんの心の中に残る大切な一本になれたら嬉しいです。
+									<br/>
+									また、素晴らしいノミネート作品の中の一つに選出していただき、とても光栄に思います。
+									<br/>
+									久保、米谷
+                                </div>
+								<br/>
+								<div class="has-text-justified has-text-periwinkle">
+									Thank you all of the fans who love Golden Kamuy and its original soundtrack. And thank you all of the organizers and the staff of Reddit 2019 Anime Awards, who always stimulate the Anime industry. By receiving the news which Golden Kamuy’s soundtrack, which includes the elements of traditional instruments, orchestra and Rock music, had been nominated, I feel excited for producing music of next season more and more. This nomination is very encouraging for me, and I would like to produce music which can make the fans of Golden Kamuy happy. Hinna Hinna!!
+                                </div>
+								<br/>
+								<h1 class="title is-5 has-text-gold has-text-centered has-flaired-overunder pb-20">Maiya Kubo</h1>
+								<div class="has-text-justified has-text-periwinkle">
+									ご連絡ありがとうございます。
+									<br/>
+									「星合の空」はまだ未完ですが、ノミネートしていただき大変光栄に思います。
+									<br/>
+									この作品の制作に関わったスタッフを代表してお礼を申し話あげます。
+									<br/>
+									またこの作品に投票してくださった「星合の空」を応援していただいている視聴者の皆さんに深く感謝を申し上げます。
+									<br/>
+									ありがとうございました。
+									<br/>
+									日本語での返信、お許しください。
+									<br/>
+									赤根和樹
+                                </div>
+								<br/>
+								<div class="has-text-justified has-text-periwinkle">
+									Everyone, thank you for watching Totsukuni no Shoujo. We made it while staying as faithful to the atmosphere of the original work as we could. It may be a short, 10-minute long work, but we're happy if it's something that you enjoyed and that stayed with you all after you watched it. Also, we consider it a great honour to be nominated alongside other such fantastic anime.
+                                </div>
+								<br/>
+								<h1 class="title is-5 has-text-gold has-text-centered has-flaired-overunder pb-20">Kazuki Akane</h1>
+								<div class="has-text-justified has-text-periwinkle">
+									ご連絡ありがとうございます。
+									<br/>
+									「星合の空」はまだ未完ですが、ノミネートしていただき大変光栄に思います。
+									<br/>
+									この作品の制作に関わったスタッフを代表してお礼を申し話あげます。
+									<br/>
+									またこの作品に投票してくださった「星合の空」を応援していただいている視聴者の皆さんに深く感謝を申し上げます。
+									<br/>
+									ありがとうございました。
+									<br/>
+									日本語での返信、お許しください。
+									<br/>
+									赤根和樹
+                                </div>
+								<br/>
+								<div class="has-text-justified has-text-periwinkle">
+									Thank you for your correspondence.
+									<br/>
+									Although Hoshiai no Sora is still unfinished, it is a great honour to be nominated.
+									<br/>
+									On behalf of the staff who worked on this show I would like to give my thanks.
+									<br/>
+									Additionally, we would like to give our sincere thanks to all the fans of Hoshiai no Sora who voted for this anime.
+									<br/>
+									Thank you.
+									<br/>
+									My apologies for responding in Japanese.
+									<br/>
+									Akane Kazuki
+                                </div>
+								<br/>
+								<h1 class="title is-5 has-text-gold has-text-centered has-flaired-overunder pb-20">Jun'ichi Sato</h1>
+								<div class="has-text-justified has-text-periwinkle">
+									『HUGっと！プリキュア』は、子供たちに向けて、誠意を持って創作しました。子供達以外の人もまた感動してくれて、この作品を好きになってくれた事が、私たちはとてもうれしいです。ありがとうございます。佐藤順一より。
+                                </div>
+								<br/>
+								<div class="has-text-justified has-text-periwinkle">
+									HUGtto! Precure" is aimed at children, but made with sincerity. That people other than children might be moved by it, and come to like this anime, makes us very happy. Thank you very much. From Jun'ichi Sato
                                 </div>
                             </div>
                         </section>
