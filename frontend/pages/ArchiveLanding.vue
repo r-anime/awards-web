d59ca7eca54c4f64d9f76cb5886e638162e591d1
EpicTroll1365 <acciowand200@hotmail.com>
2020-03-10 11:04:09 +0200

    a whatever interface for archive landing

--- frontend/pages/ArchiveLanding.vue
+++ frontend/pages/ArchiveLanding.vue
@@ -1,17 +1,49 @@
 <template>
     <div class="has-background-dark">
-    <section class="hero is-fullheight-with-navbar">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <div class="column is-two-thirds-desktop is-three-quarters-tablet is-four-fifths-mobile has-background-black-bis">
-                        <router-link to="/archive/2018" style="color:inherit">
-                        <button class="button is-primary">2018</button>
-                        </router-link>
+        <section class="hero is-fullheight-with-navbar">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns is-centered">
+                        <div class="column is-four-fifths-desktop is-three-quarters-tablet is-four-fifths-mobile has-background-black-bis">
+                            <section class="section">
+                                <div class="container">
+                                    <h2 class="title is-2 has-text-gold has-text-centered has-flaired-underline pb-20">2018</h2>
+                                    <div class="control has-text-centered">
+                                        <router-link to="/archive/2018" style="color:inherit">
+                                            <button class="button is-large is-platinum is-outlined mb-10">Full Results</button>
+                                        </router-link>
+                                    </div>
+                                    <br/>
+                                    <div class="has-text-centered has-text-light">
+                                        <b>Hosts:</b> /u/codelelouche, /u/drjwilson, /u/malacostracaflame, /u/metasoshi9, /u/mpp00, /u/pandavengerx, /u/patureau, /u/ralon17, /u/reyae, /u/__u_wot_m8__, /u/vaxivop
+                                    </div>
+									<br/>
+                                    <div class="has-text-centered has-text-light">
+                                        <b>Livestream Guests:</b> YonkouProductions, Pause and Select, AnimePhilosopher, Aleczandxr, aTonofBacon, basedbecker, KevinNyaa, Zeria, Ember Reviews, MozillaFennekin (Lux), ShaKing807, Mathwiz97, Hiding in Public, PhenomSage, RogerSmith2004, TheCanipaEffect, SubtitledAnime, FateSteelTaylor, Kor Reviews, ThePedanticRomantic, LilyPichu
+                                    </div>
+									<br/>
+                                    <div class="has-text-centered has-text-light">
+                                        <b>Livestream Editors:</b> /u/clerikal, /u/commandersevan, /u/jonlxh, /u/nickknight8, /u/irvom, /u/zhongzhen93
+                                    </div>
+                                    <br/>
+                                    <h2 class="title is-2 has-text-gold has-text-centered has-flaired-underline pb-20">Other Credits</h2>
+                                    <div class="has-text-centered has-text-light">
+                                        <b>This archive of past results was only made possible through the tireless efforts of these volunteers:</b>
+                                    </div>
+									<br/>
+                                    <div class="has-text-centered has-text-light">
+                                    /u/AdiMG, /u/Chem_chem, /u/irvom, /u/thyeggman, /u/unprecedentedwolf, /u/WillQ
+                                    </div>
+                                    <br/>
+                                    <div class="has-text-justified has-text-periwinkle">
+                                        The /r/anime Awards have been an ongoing volunteer efforts for 4 years in a row now. The strong foundation that we've built over these years wouldn't be possible without the invaluable help of the /r/anime mod-team and the indispensable feedback of the users of /r/anime. The /r/anime Awards would like to thank every member of /r/anime who voted and participated in these Awards throughout the year, whether as jurors or members of the public. We hope to see you again in the future!
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     </div>
 </template>
