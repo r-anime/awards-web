70c052c93d8968e193f46b672b3a94bfaaf2c23b
devbaj <bjory7@gmail.com>
2020-03-16 15:01:59 -0700

    added styling

--- frontend/pages/ExtraAwards.vue
+++ frontend/pages/ExtraAwards.vue
@@ -4,35 +4,39 @@
       <section class="hero is-fullheight-with-navbar">
         <div class="hero-body">
           <div class="container">
-            <div class="column is-centered">
+            <div class="columns is-centered">
               <div class="column is-two-thirds-desktop is-three-quarters-tablet is-four-fifths-mobile has-background-black-bis">
-                <h1 class="title is-3 has-text-llperiwinkle has-text-left">Extra Awards</h1>
-                <div class="has-text-left has-text-light">
-                  Extra Awards are a chance to be looser and have fun with categories that may be less prestigious, but are nonetheless an important celebration of what anime fans love.
-                </div>
-                <table>
-                  <thead>
-                    <tr>
-                      <td>Award</td>
-                      <td>Winner</td>
-                      <td>Runner-up</td>
-                    </tr>
-                  </thead>
-                  <tr
-                    v-for="a in awards"
-                    :key="a.category"
-                  >
-                    <td>{{a.category}}</td>
-                    <td>
-                      <a v-if="a.winner.link" :href=a.winner.link>{{a.winner.name}}</a>
-                      <div v-else>{{a.winner.name}}</div>
-                    </td>
-                    <td>
-                      <a v-if="a.runnerup.link" :href=a.runnerup.link>{{a.runnerup.name}}</a>
-                      <div v-else>{{a.runnerup.name}}</div>
-                    </td>
-                  </tr>
-                </table>
+                <section class="section">
+                  <div class="container">
+                    <h1 class="title is-3 has-text-llperiwinkle has-text-left">Extra Awards</h1>
+                    <div class="has-text-left has-text-light">
+                      Extra Awards are a chance to be looser and have fun with categories that may be less prestigious, but are nonetheless an important celebration of what anime fans love.
+                    </div>
+                    <table class="table has-background-black-bis">
+                      <thead class="has-text-weight-bold">
+                        <tr>
+                          <td>Award</td>
+                          <td>Winner</td>
+                          <td>Runner-up</td>
+                        </tr>
+                      </thead>
+                      <tr
+                        v-for="a in awards"
+                        :key="a.category"
+                      >
+                        <td>{{a.category}}</td>
+                        <td>
+                          <a v-if="a.winner.link" :href=a.winner.link>{{a.winner.name}}</a>
+                          <div v-else>{{a.winner.name}}</div>
+                        </td>
+                        <td>
+                          <a v-if="a.runnerup.link" :href=a.runnerup.link>{{a.runnerup.name}}</a>
+                          <div v-else>{{a.runnerup.name}}</div>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </section>
               </div>
             </div>
           </div>
@@ -46,14 +50,13 @@
 import data2019 from '../../data/results2019.json';
 
 export default {
-  data () {
-    return {
-      awards: data2019.extras,
-  };
-  },
-  mounted () {
-    window.scroll(0, 0);
-    console.log(this.awards);
-  }
+	data () {
+		return {
+			awards: data2019.extras,
+		};
+	},
+	mounted () {
+		window.scroll(0, 0);
+	},
 };
 </script>
