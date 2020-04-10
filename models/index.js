ba3b7ad6eed4eb21456b329d2a0826e2fe9b1b7f
EpicTroll1365 <acciowand200@hotmail.com>
2020-04-10 15:46:45 +0300

    use config parameters and clean up this file

--- models/index.js
+++ models/index.js
@@ -4,26 +4,26 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
+const config = require('../config');
+
 const sequelize = new Sequelize({
 	dialect: 'sqlite',
-	storage: '../awards.db',
+	storage: path.join(config.db.path, config.db.filename),
 });
 
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+	.readdirSync(__dirname)
+	.filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+	.forEach(file => {
+		const model = sequelize.import(path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
