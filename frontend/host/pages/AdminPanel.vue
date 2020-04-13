dae3c5e96c2c02c7c2f1c9018de22adc22d41a8e
EpicTroll1365 <acciowand200@hotmail.com>
2020-04-13 16:10:14 +0300

    error handling

--- frontend/host/pages/AdminPanel.vue
+++ frontend/host/pages/AdminPanel.vue
@@ -222,7 +222,6 @@ export default {
 						if (nom.anilist_id !== '') anime.push(nom.anilist_id);
 					}
 				}
-				console.log(chars);
 				const showPromise = new Promise(async (resolve, reject) => {
 					try {
 						let showData = [];
@@ -266,12 +265,8 @@ export default {
 						data.anime[`${show.id}`] = show.title.romaji;
 					}
 					for (const char of allChars) {
-						console.log(char);
-						if (!char.media.edges) console.log(`${char} is the culprit.`);
-						if (!char) console.log(`${char} is the culprit.`);
-						if (!char.media) console.log(`${char} is the culprit.`);
 						let va = '';
-						if (char.media.edges[0].voiceActors[0]) va = char.media.edges[0].voiceActors[0].name.full;
+						if (char.media.edges.length > 0) va = char.media.edges[0].voiceActors[0].name.full;
 						data.characters[`${char.id}`] = {
 							name: char.name.full,
 							anime: char.media.nodes[0].title.romaji,
