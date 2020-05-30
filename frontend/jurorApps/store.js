25d1fb388b4c5467c42d13775e1151594e51add0
JoseiToAoiTori <acciowand200@hotmail.com>
2020-05-30 14:23:33 +0300

    set up authentication flow for apps

---
+++ frontend/jurorApps/store.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-await-in-loop */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+async function makeRequest (path, method = 'GET', body) {
+	if (typeof body === 'object' && body != null) {
+		body = JSON.stringify(body);
+	}
+	try {
+		const result = await fetch(path, {method, body});
+		if (!result.ok) {
+			const json = await result.json();
+			throw json.error;
+		}
+		if (result.status === 204) {
+			return;
+		}
+		return await result.json();
+	} catch (error) {
+		// eslint-disable-next-line no-alert
+		window.alert(error);
+		throw error;
+	}
+}
+
+const store = new Vuex.Store({
+	state: {
+		me: null,
+	},
+	getters: {
+		isHost (state) {
+			return state.me && state.me.level >= 2;
+		},
+		isMod (state) {
+			return state.me && state.me.level >= 3;
+		},
+		isAdmin (state) {
+			return state.me && state.me.level >= 4;
+		},
+	},
+	mutations: {
+		GET_ME (state, me) {
+			state.me = me;
+		},
+	},
+	actions: {
+		async getMe ({commit}) {
+			const response = await fetch('/api/user/me');
+			if (!response.ok) return;
+			const me = await response.json();
+			commit('GET_ME', me);
+		},
+	},
+});
+
+export default store;
