8d54cdeaf069704d4e9e7fe1c8102067d279f3f3
Geo <georgej1088@gmail.com>
2020-08-12 03:30:28 -0400

    update to css-loader@4 (#111)
    * update to css-loader@4
    
    * remove irrelevant comment

--- webpack.config.js
+++ webpack.config.js
@@ -57,15 +57,24 @@ module.exports = {
 				test: /\.css$/,
 				use: [
 					'vue-style-loader',
-					'css-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							esModule: false,
+						},
+					},
 				],
 			},
 			{
 				test: /\.scss$/,
 				use: [
 					'vue-style-loader',
-					// so for some reason bumping css loader kills all our css. look into this later.
-					'css-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							esModule: false,
+						},
+					},
 					{
 						loader: 'sass-loader',
 						options: {
