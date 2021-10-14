a9cb4f6e6a1cd73ad5a3bc591c9131416766ba78
pandavenger <pandavengerx@gmail.com>
2021-10-13 20:35:58 -0400

    oh right

--- routes/application-api.js
+++ routes/application-api.js
@@ -772,15 +772,19 @@ apiApp.get('/my-answers/:applicant_id', async (request, response) => {
 		return response.json(401, {error: 'Invalid user.'});
 	}
 	try {
-		response.json(await Answers.findAll({
+		const answers = await Answers.findAll({
 			where: {
 				applicant_id: request.params.applicant_id,
 				include: [{
 					model: Questions,
 					as: 'question',
+					where: {
+						active: true,
+					}
 				}]
 			},
-		}));
+		});
+		response.json(answers);
 	} catch (error) {
 		response.error(error);
 	}
