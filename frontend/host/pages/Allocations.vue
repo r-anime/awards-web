f9dd91bf0b96a42c335470359fa16a8d3e573856
pandavenger <pandavengerx@gmail.com>
2021-10-28 01:54:36 -0400

    i'm sleepy sorry

--- frontend/host/pages/Allocations.vue
+++ frontend/host/pages/Allocations.vue
@@ -190,7 +190,7 @@ export default {
 			this.calculateStats();
 		}
 		const namesLock = this.locks.find(lock => lock.name === 'app-names');
-		if (namesLock.flag || this.me.level > lock.level) {
+		if (namesLock.flag || this.me.level > namesLock.level) {
 			this.showNames = true;
 		}
 		this.loaded = true;
