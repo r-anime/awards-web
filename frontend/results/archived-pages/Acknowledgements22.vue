18c65cdc80dd953ec1a8a823c622b4faf44a5803
pandavenger <pandavengerx@gmail.com>
2024-03-04 20:05:36 -0500

    page

---
+++ frontend/results/archived-pages/Acknowledgements22.vue
@@ -0,0 +1,173 @@
+<template>
+<div class="">
+	<div class="container">
+		<section class="section">
+			<div class="container">
+				<h1 class="title is-2 has-text-gold has-text-left pb-20">Acknowledgements</h1>
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Kensuke Ushio (牛尾憲輔)</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						Thank you for selecting us to win the award for best OST.
+						<br/><br/>
+						I am extremely pleased that you all enjoyed it.
+						<br/><br/>
+						The OST for Chainsaw Man is the product of a number of challenges.
+						<br/><br/>
+						What kind of anime should "Chainsaw Man" be? I spent a long time continuously thinking about this, continuously experimenting through trial and error. It's a huge source of encouragement to me that the results resonated with you all like this.
+						<br/><br/>
+						I would also like to extend my thanks to the production team for permitting me to take on these challenges.
+						<br/><br/>
+						「THAT'S A DREAM COME TRUE....」
+						<br/><br/>
+						So, when're we doing the AMA?
+						<br/><br/>
+						Kensuke Ushio
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						サントラ賞受賞、ありがとうございます。
+						<br/><br/>
+						皆さんに喜んで頂けたことがとても嬉しいです。
+						<br/><br/>
+						チェンソーマンのサウンドトラックは様々な挑戦の産物です。
+						<br/><br/>
+						アニメ"チェンソーマン"とはどんな作品なのか、考え続け試行錯誤し続けた長い時間がこうして皆さんに響いたことがとても励みになります。
+						<br/><br/>
+						また、こうした挑戦を許してくださった制作チームにも感謝します。
+						<br/><br/>
+						THAT'S A DREAM COME TRUE....
+						<br/><br/>
+						で、AMAいつやろっか？
+						<br/><br/>
+						牛尾憲輔
+					</div>
+				</div>
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Yuki Kamiya (神谷雄貴) - Chainsawman ED3 Director</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						I feel like the warm reception the ED has received is in large part thanks to the support from the MAPPA team in making the Chainsaw Man anime itself so appealing, and the strength of the music composition. Even still, when I think about how I've managed to contribute to the excitement around Chainsaw Man through my own form of expression, I'm filled with a feeling of pride.
+						<br/><br/>
+						I'm going to work hard from now on to create more works that will stay in many people's hearts.
+						<br/><br/>
+						Kamiya Yuki
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						このED映像が評価いただけたのは、
+						<br/>
+						チェンソーマンという作品自体の魅力と楽曲の力強さ、
+						<br/>
+						MAPPAチームのサポートによるところが大きいと感じます。
+						<br/>
+						それでも、自分なりの表現でチェンソーマンの盛り上がりの一役を担えたと思うと
+						<br/>
+						誇らしい気持ちでいっぱいです。
+						<br/><br/>
+						今後も多くの方の心に留まる作品を作れるように頑張ります。
+						<br/><br/>
+						神谷雄貴
+					</div>
+				</div>
+
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Yuuichirou Kikuchi (菊池雄一郎) - Kaguya-sama Animation Producer</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						Please accept my thanks for the kind support you all have given us, and for selecting us to win the award for Best Romance.
+						<br/>
+						For those of us on the production team who have been working on Kaguya-sama for so long, to receive this support from you all feels like a great honor.
+						<br/><br/>
+						We never could have imagined that our Japanese animation would be seen by people all over the world, and be appreciated this much. I myself do not think I have contributed that greatly to Kaguya-sama. I believe it was the efforts and superb talents of the whole staff that made it possible to convey its emotions to you all.
+						<br/><br/>
+						In particular, Mamoru Hatakeyama contributed greatly, and without his talent I do not think it would have been possible to bring this anime to life. 
+						He does not often appear in the media, but he is always reading your encouraging and supportive comments and greatly appreciates them. I would like to convey the news about this award to him as well.
+						<br/><br/>
+						I am working on a number of upcoming projects, but I am also hoping to come out with the continuation of Kaguya-sama if you all could continue to support it. So I would greatly appreciate if you could continue to support Kaguya-sama and look forward to A-1 Pictures' future productions!
+						<br/><br/>
+						Thank you so much!
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						今回皆様からの温かいご支援により、恋愛賞を受賞いたしましたことお礼申し上げます。
+						<br/>
+						長らくかぐや様に携わってきまして、こうして皆様に支持いただけることは我々制作スタッフ一同名誉あることと感じております。
+						<br/><br/>
+						世界の皆様に日本のアニメを見ていただき、こうして評価いただけることは想像できていなかったことであります。
+						私自身はかぐや様にそこまで大きな貢献してきたとは思っておりません。
+						スタッフ一同の努力と素晴らしい才能が、皆様に感動を伝えられたのだと思っております。
+						<br/><br/>
+						特に畠山守監督の功績は大きく、彼の才能なしではこの作品は実現できなかったと思います。
+						メディアには出ない方ですが、皆様の励ましや応援のコメントはいつも見て喜んでおりました。
+						今回の受賞もお伝えさせていただければと思います。
+						<br/><br/>
+						今後は様々な作品を手掛けていきますが、かぐや様も皆様からのさらなる応援がありましたら続編も実現できると思っております。
+						なので是非これからもかぐや様への応援とA-1 picturesへ期待をしていただきたいです！
+						<br/><br/>
+						本当にありがとうございました！
+					</div>
+				</div>
+
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Ryuu Nakayama (中山竜) - Chainsawman Director</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						Thank you for watching "Chainsaw Man".
+						<br/>
+						It's a great honor to receive the award for Best Action and Best Cinematography.
+						<br/>
+						Moreover, I am extremely happy that so many people all over the world enjoyed the show.
+						<br/>
+						We will be taking on a number of new challenges in our upcoming projects as well.
+						<br/><br/>
+						Thank you very much.
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						アニメ『チェンソーマン』をご覧頂きましてありがとうございます。
+						<br/>
+						アクション賞と撮影賞をいただくことができ光栄に思っています。
+						<br/>
+						また、世界中で多くの方々に楽しんでいただけたことを大変嬉しく思います。
+						<br/>
+						次回作以降も様々なことに挑戦していきます。
+						<br/>
+						この度はまことにありがとうございました。
+					</div>
+				</div>
+
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Tomohiro Furukawa (古川知宏) - Revue Starlight Director</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						We exhaustively designed this anime to be seen in theatres, so please do go and see it in a cinema, even if you've seen it already. It's had almost no screenings overseas, so if possible please rent out a cinema so you can watch it on a big screen with a good sound system.<br/>
+						I promise you that it's a completely different film compared to watching it at home.<br/>
+						I extend my thanks to the staff and cast who worked on the film, and the fans who supported us.<br/>
+						Thank you.
+						<br/><br/>
+						Furukawa Tomohiro
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						この作品は完全に「映画館で観るため」にあらゆる設計がなされている作品ですので、既にご覧になられた方も、出来れば映画館でご鑑賞下さい。海外では上映される事が殆ど無かった作品ですので、可能でしたら映画館をレンタルして皆で大きなスクリーンと音で観て下さい。<br/>
+						自宅で観たものとは「全く別の映画」になる事をお約束します。<br/>
+						作品に関わってくれたスタッフとキャスト、そして応援してくれたファンに感謝しております。<br/>
+						ありがとうございました。
+						<br/><br/>
+						古川知宏
+					</div>
+				</div>
+
+				<h2 class="title is-5 has-text-gold has-text-centered pb-20">Kazuyoshi Mukaitouge (向峠和喜) - DIY Animation Producer</h2>
+				<div class="columns mb-6">
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						It is a great honour that the show our company produced, Do It Yourself, has been this well received.
+						<br/><br/>
+						Generally in character design, it's quite common to portray cute female characters as having large eyes. But it's precisely because the character designer, Yuusuke Matsuo, has been drawing these kinds of characters for so long that he was not only able to make a character look cute even with small eyes, but design them with an immediately apparent sense of uniqueness.<br/>
+						By the way, Jobko was inspired by Steve Jobs.
+					</div>
+					<div class="column has-text-centered has-text-periwinkle is-flex is-align-items-center">
+						弊社制作のDo It Yourselfでそのように評価を頂くことができまして、大変光栄に思います。
+						<br/><br/>
+						キャラクターデザインに関しましては一般的に目が大きいのが可愛い女の子のキャラクターとして描写されることが多かったのですが<br/>
+						そういったキャラクターを描き続けてきたキャラクターデザインの松尾祐輔だからこそ<br/>
+						瞳を小さくしても可愛く見せれて、さらにデザインを見るだけで個性を感じれるキャラクターを作ることができたんだと思います。<br/>
+						ちなみにジョブ子はスティーブ・ジョブズからインスピレーションを受けてます。<br/>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+</template>
