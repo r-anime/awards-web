51538159db104397b2a1efa3d9f75ad81957eb70
pandavenger <pandavengerx@gmail.com>
2024-03-08 22:32:30 -0500

    actual page lol

---
+++ frontend/results/archived-pages/About22.vue
@@ -0,0 +1,110 @@
+<template>
+<section class="section">
+    <section class="hero is-fullheight-with-navbar">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-two-thirds-desktop is-three-quarters-tablet is-four-fifths-mobile">
+                        <section class="section">
+                            <div class="container">
+                                <h1 class="title is-3 has-text-llperiwinkle has-text-left">Credits</h1>
+                                <div class="has-text-justified has-text-light">
+                                    The /r/anime Awards, this website and the Awards Livestream are a labor of love made possible through the many volunteers involved in the entire process.
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                    We’d like to start by thanking /u/chonpalm, /u/eritbh, /u/hauntmeagain, /u/JoseiToAoiTori, and /u/PandavengerX for their work in creating this website and for putting up with unending changes, edits and last-minute requests. We would also like to thank them for their work on the websites for juror applications, initial voting, final voting and the internal website used by hosts and /r/anime moderators to manage the Awards and see results.
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                    We’d like to thank all of our livestream guests for agreeing to be a part of this year’s most ambitious endeavor. You all have plenty of your own work but you were willing to help in making our results discussion more varied and insightful. We hope it was a pleasant rather than taxing experience!
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                    We’d like to thank /u/DrJWilson, /u/paukshop, /u/misterjaguar, /u/MyrnaMountWeazel, and /u/unprecedentedwolf specifically for being willing to take up the monumental task of livestream host, along with all of the coordinating, behind-the-scenes work, and stamina that the role involves. The fact that Wilson is also rumored to be the WORLD FAMOUS YOUTUBER KAMIMASHITA is just icing on the cake.
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                    We’d like to thank all of the people who put work into the production of the livestream:
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                    We’d like to thank all the people who volunteered to edit jury write-ups:
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                   We’re thankful to the moderators of r/anime for their support and in many cases active cooperation with something as involved as the awards.
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                   We’re also very appreciative of the users who committed to immense task of hosting this year.
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                   The true soul of the r/Anime Awards is the evaluation of each year’s anime, and from the jury side of things it is not an easy task. We want to thank all of the 90+ jurors who signed up to watch, discuss, and write about the enormous number of selections available this year. It was a blast to work with all of them and we hope to see some familiar faces apply again next year!
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                   Finally we would also like to thank you, the public! Without your support these awards would not be able to come to fruition, and you are both the participants and the audience for whom these awards are run. We hope you join us on this journey again next year!
+                                </div>
+                                <br/>
+                                <div class="has-text-justified has-text-light">
+                                   Complete Livestream Guest List (in order of appearance):
+                                </div>
+                                <div class="content has-text-light">
+                                   <ul>
+                                        <li>Anime Addicts Anonymous</li>
+                                        <li>Inakyu</li>
+                                        <li>Spilling the Milk (Kor and Jackisboy)</li>
+                                        <li>Pause and Select</li>
+                                        <li>Shaking807 and FateSteelTaylor</li>
+                                        <li>Frog-kun</li>
+                                        <li>KevinNyaa</li>
+                                        <li>EmberReviews</li>
+                                        <li>Red Bard</li>
+                                        <li>The Canipa Effect</li>
+                                        <li>ultimatemegax</li>
+                                        <li>Shaybs</li>
+                                        <li>Anime Music Podcast (Kor and Shabu)</li>
+                                        <li>Megan Shipman</li>
+                                        <li>Replay Value</li>
+                                        <li>The Cynic Clinic and GilLiesHere</li>
+                                        <li>PhenomSage</li>
+                                        <li>Mozilla Fennekin and Sinrus (Tokyo Podfathers)</li>
+                                   </ul>
+                                </div>
+                                <br/>
+                                <h2 class="title is-4 has-text-llperiwinkle has-text-left">Commissioned Artwork</h2>
+                                <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center has-text-light">
+                                    <img loading="lazy" src="img/snoo_hyeinlee.png" height="820" width="432"/>
+                                    <br/>
+                                    <a class="has-text-light is-size-5 mt-2 my-6" href="https://twitter.com/animehyein">Hye In Lee </a>
+                                    <img loading="lazy" src="img/snoo_kazuhitopanda.png" height="820" width="432"/>
+                                    <br/>
+                                    <a class="has-text-light is-size-5 mt-2 my-6"  href="https://twitter.com/Panda23577667">Panda23577667 </a>
+                                    <img loading="lazy" src="img/snoo_kenichiroaoki.png" height="820" width="432"/>
+                                    <br/>
+                                    <a class="has-text-light is-size-5 mt-2 my-6"  href="https://twitter.com/jumpaoki" >Kenichirou Aoki</a>
+                                    <img loading="lazy" src="img/snoo_naoyamorotomi.png" height="820" width="432"/>
+                                    <br/>
+                                    <a class="has-text-light is-size-5 mt-2 my-6"  href="https://twitter.com/708FNWP"> Naoya Morotomi </a>
+                                    <img loading="lazy" src="img/snoo_futata.png" height="820" width="432"/>
+                                    <br/>
+                                    <a class="has-text-light is-size-5 mt-2 my-6"  href="https://twitter.com/fttuaa"> Futata </a>
+                                </div>
+				                <h2 class="title is-4 has-text-llperiwinkle has-text-left">The Jury Process</h2>
+				                <div class="has-text-justified has-text-light">
+                                    Below is the process the jury took this year to decide their nominees, rankings and write-ups. This follows ~2 months of dedicated pre-planning, making these awards a near-exact 6 month undertaking! 
+                                </div>
+                                <div class="hero is-fullheight-with-navbar section has-background-dark">
+                                    <iframe style="height: 100vh; width: 100%;" src="https://docs.google.com/document/d/e/2PACX-1vS2qIX21V-9yU4FUwAbMGrF-VSNBkCAzmGsZBwhzhFAw_8gPO3oakyX3_uzmESoxw/pub?embedded=true"></iframe>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+</template>
