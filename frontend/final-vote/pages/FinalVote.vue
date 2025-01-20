bb9dbc42192d43380d2d98cb7d6ad606086a2e48
pandavenger <pandavengerx@gmail.com>
2025-01-19 22:24:00 -0500

    fix sorting

--- frontend/final-vote/pages/FinalVote.vue
+++ frontend/final-vote/pages/FinalVote.vue
@@ -207,7 +207,7 @@ export default {
 				// eslint-disable-next-line eqeqeq
 				const noms = this.unique.survey;
 				noms.sort((a, b) => {
-					if (a.id < 0){
+					if (a.id < 0 || b.id < 0){
 						return 0;
 					}
 					if (this.getName(a).trim() < this.getName(b).trim()) {
