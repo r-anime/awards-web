7dc90ecd4deb83daef3bf7e97dfe38ebe86bb20c
pandavenger <pandavengerx@gmail.com>
2025-03-08 22:46:07 -0500

    fixes

--- frontend/common/navbar-routes.js
+++ frontend/common/navbar-routes.js
@@ -1,7 +1,6 @@
 const resultsRoutes = [
 	{name: 'Results', path: '/results/all', children: [
 		{name: 'Genre', path: '/results/genre'},
-		{name: 'Character', path: '/results/character'},
 		{name: 'Production', path: '/results/production'},
 		{name: 'Main', path: '/results/main'},
 	]},
