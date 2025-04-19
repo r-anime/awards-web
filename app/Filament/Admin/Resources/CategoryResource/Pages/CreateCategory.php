6cf705d4a7e18ba3174a464aad0e2ced1e6f9889
Kei74 <keilumoonati74@gmail.com>
2025-04-19 19:50:59 +0530

    Category resource basic table and form

---
+++ app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Admin\Resources\CategoryResource\Pages;
+
+use App\Filament\Admin\Resources\CategoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCategory extends CreateRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // TODO: implement auto-determined order
+        $data['order'] = 5;
+
+        return $data;
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Unguarding category model as per filament doc
+        Category::unguard();
     }
 }
