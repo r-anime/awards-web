ec58d81cd405e5bd6047787e4c4589b657cca426
pandavenger <pandavengerx@gmail.com>
2026-03-08 21:56:01 -0400

    image fix

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,6 +1,6 @@
 export const nomineeImage = (nominee) => {
         try {
-            return `background-image: url(/${nominee.image})`;        
+            return `background-image: url(/storage/${nominee.image})`;        
         } catch (error) {
             console.log('Error with image url');
             console.log(nominee);
