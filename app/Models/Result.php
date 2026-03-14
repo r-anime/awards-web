19426f85dde211a32cbc0c80018410e386b4f5c2
Kei74 <95759937+Kei74@users.noreply.github.com>
2026-03-14 10:17:35 +0530

    auto clear result cache (#235)

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\ResultObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy([ResultObserver::class])]
 class Result extends Model
 {
     use HasFactory;
